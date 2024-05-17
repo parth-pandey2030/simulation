5bfcec9209f51936ec363d69f8c7ef01c336cff8
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-05-16 21:11:14 -0400

    Thu May 16 21:11:14 EDT 2024

---
+++ divergenceAndCurl.py
@@ -0,0 +1,5 @@
+# Divergence (symbol: div) and curl (symbol: curl)
+# are used in Maxwell's Equations, which are used 
+# to described electromagnetism.
+# Go here for more info: 
+# https://www.youtube.com/watch?v=rB83DpBJQsE
