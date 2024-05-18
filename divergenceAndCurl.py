bfdd4b98d0b6e537d2628eb29c4f6ff79584f828
parth-pandey7000 <147671693+h6ker2969@users.noreply.github.com>
2024-05-18 10:10:30 -0400

    Sat May 18 10:10:30 EDT 2024

--- divergenceAndCurl.py
+++ divergenceAndCurl.py
@@ -3,3 +3,18 @@
 # to described electromagnetism.
 # Go here for more info: 
 # https://www.youtube.com/watch?v=rB83DpBJQsE
+from sympy import symbols, diff 
+
+def div(vector):
+    f = vector[0]
+    g = vector[1]
+    h = vector[2]
+    return diff(f, symbols('x')) + diff(g, symbols('y')) + diff(h, symbols('z'))
+
+def curl(vector):
+    f = vector[0]
+    g = vector[1]
+    h = vector[2]
+    i = j = k = 1j
+    return (diff(f, symbols('y')) - diff(g, symbols('z'))) * i - (diff(h, symbols('x')) - diff(f, symbols('z'))) * j + (diff(g, symbols('x')) - diff(f, symbols('y'))) * k 
+
--- thefundamentalforces.py
+++ thefundamentalforces.py
@@ -1,11 +1,14 @@
-from scipy.constants import speed_of_light, G, pi 
+from scipy.constants import speed_of_light, G, pi, pico, micro
 from divergenceAndCurl import *
+from sympy import symbols, diff
+
 class Gravity:
     """
     Einstien field equation
     Ruv - 1/2Rguv = 8piG/c^4Tuv
     """
-    k = (8 * pi * G) / (speed_of_light ** 4)
+
+    k = (8 * pi * G) / (speed_of_light ** 4) # Einstien Gravitational Constant
     def __init__(self, spacetime):
         self.spacetime = spacetime
         
@@ -15,12 +18,39 @@ class Gravity:
 class Electromagnetism:
     """
     Maxwell's equations
-    ∇*E = ρ/𝜀0
-    ∇
-    ∇
-    ∇
-    ∇
+    ∇·E = ρ/𝜀0
+    ∇·B = 0
+    ∇×E = -∂B/∂t
+    ∇×B = μ0(J+𝜀0∂E/∂t)
     """
+
+    def __init__(self, electric_field, magnetic_field, charge_of_surface, volume_of_surface, velocity_of_charges, operation):
+        self.electric = electric_field
+        self.magnetic = magnetic_field
+        self.charge = charge_of_surface
+        self.volume = volume_of_surface
+        self.velocity = velocity_of_charges
+        self.op = operation
+        return self.calculate()
+
+    def calculate(self):
+        𝜀0 = 8.8541878128(13) * pico # Vacuum Permittivity
+        μ0 = 1.25663706212(19) * micro # Magnetic Constant
+        ρ = self.charge / self.volume # Electric Charge Density
+        j = ρ * self.velocity # Current Density
+
+        if self.op == div(self.electric):
+            return ρ / 𝜀0
+        elif self.op == div(self.magnetic):
+            return 0
+        elif self.op == curl(self.electric):
+            return -(diff(self.magnetic, symbols('t')))
+        elif self.op == curl(self.magnetic):
+            return μ0 * (j + 𝜀0 * diff(self.electric, symbols('t')))
+        
+        
+
+
 
 
 
